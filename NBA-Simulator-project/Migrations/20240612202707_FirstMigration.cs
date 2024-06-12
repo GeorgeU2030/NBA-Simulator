9499641fd53c49f6140c9c7168cf8ae28f933396
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-12 15:29:27 -0500

    feat: migrations has been added

---
+++ NBA-Simulator-project/Migrations/20240612202707_FirstMigration.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NBA_Simulator_project.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Logo = table.Column<string>(type: "text", nullable: false),
+                    Conference = table.Column<string>(type: "text", nullable: false),
+                    Division = table.Column<string>(type: "text", nullable: false),
+                    Rings = table.Column<int>(type: "integer", nullable: false),
+                    TitlesConference = table.Column<int>(type: "integer", nullable: false),
+                    TitlesDivision = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.TeamId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seasons",
+                columns: table => new
+                {
+                    SeasonId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Edition = table.Column<int>(type: "integer", nullable: false),
+                    ChampionId = table.Column<int>(type: "integer", nullable: true),
+                    SubChampionId = table.Column<int>(type: "integer", nullable: true),
+                    ChampionEastId = table.Column<int>(type: "integer", nullable: true),
+                    ChampionWestId = table.Column<int>(type: "integer", nullable: true),
+                    SemiFinalistEastId = table.Column<int>(type: "integer", nullable: true),
+                    SemiFinalistWestId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seasons", x => x.SeasonId);
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_ChampionEastId",
+                        column: x => x.ChampionEastId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_ChampionId",
+                        column: x => x.ChampionId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_ChampionWestId",
+                        column: x => x.ChampionWestId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_SemiFinalistEastId",
+                        column: x => x.SemiFinalistEastId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_SemiFinalistWestId",
+                        column: x => x.SemiFinalistWestId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Seasons_Teams_SubChampionId",
+                        column: x => x.SubChampionId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Match",
+                columns: table => new
+                {
+                    MatchId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SeasonId = table.Column<int>(type: "integer", nullable: false),
+                    HomeTeamId = table.Column<int>(type: "integer", nullable: false),
+                    VisitorTeamId = table.Column<int>(type: "integer", nullable: false),
+                    HomeScore = table.Column<int>(type: "integer", nullable: false),
+                    VisitorScore = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Match", x => x.MatchId);
+                    table.ForeignKey(
+                        name: "FK_Match_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "SeasonId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Match_Teams_HomeTeamId",
+                        column: x => x.HomeTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Match_Teams_VisitorTeamId",
+                        column: x => x.VisitorTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SeasonTeams",
+                columns: table => new
+                {
+                    SeasonTeamId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SeasonId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Logo = table.Column<string>(type: "text", nullable: false),
+                    Wins = table.Column<int>(type: "integer", nullable: false),
+                    Losses = table.Column<int>(type: "integer", nullable: false),
+                    Percentage = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeasonTeams", x => x.SeasonTeamId);
+                    table.ForeignKey(
+                        name: "FK_SeasonTeams_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "SeasonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_HomeTeamId",
+                table: "Match",
+                column: "HomeTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_SeasonId",
+                table: "Match",
+                column: "SeasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_VisitorTeamId",
+                table: "Match",
+                column: "VisitorTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_ChampionEastId",
+                table: "Seasons",
+                column: "ChampionEastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_ChampionId",
+                table: "Seasons",
+                column: "ChampionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_ChampionWestId",
+                table: "Seasons",
+                column: "ChampionWestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_SemiFinalistEastId",
+                table: "Seasons",
+                column: "SemiFinalistEastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_SemiFinalistWestId",
+                table: "Seasons",
+                column: "SemiFinalistWestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_SubChampionId",
+                table: "Seasons",
+                column: "SubChampionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeasonTeams_SeasonId",
+                table: "SeasonTeams",
+                column: "SeasonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Match");
+
+            migrationBuilder.DropTable(
+                name: "SeasonTeams");
+
+            migrationBuilder.DropTable(
+                name: "Seasons");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
--- NBA-Simulator-project/Program.cs
+++ NBA-Simulator-project/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using NBA_Simulator_project.Data;
+using dotenv.net;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load();
 
 // Add services to the container.
 
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var postgresSQL = Environment.GetEnvironmentVariable("POSTGRES_SQL");
+
+var connectionString = postgresSQL;
+
+builder.Services.AddDbContext<NbaDb>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
